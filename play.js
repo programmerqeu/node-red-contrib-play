8357e78b8fd4938dda63e1ecc40ebfb64cc357bf
André Lademann <vergissberlin@googlemail.com>
2016-12-31 17:46:43 +0100

    Updated license informations

--- play.js
+++ play.js
@@ -3,13 +3,14 @@
  *
  * LICENSE:    MIT
  *
- * @project    node-red-node-play
- * @package    ChromeExtension
+ * @project    node-red-contrib-play
+ * @package    Programmerq\Nodered\Node\Play
  * @author     André Lademann <andre@programmerq.eu>
- * @copyright  Copyright (c) 2014 programmerq.eu (http://www.programmerq.eu)
- * @license    http://programmerq.eu/license
+ * @copyright  Copyright programmerq.eu (http://www.programmerq.eu)
+ * @license    MIT https://opensource.org/licenses/MIT
  * @since      2014-11-27 - 08:53:21 AM
  */
+
 module.exports = function(RED) {
 	'use strict';
 
@@ -22,13 +23,14 @@ module.exports = function(RED) {
 
 	function PlayNode(config) {
 		RED.nodes.createNode(this,config);
-		var node = this;
-		var options, player = require('play-sound')(options = {player: this.type});
+		var node = this,
+			  options,
+		    player = require('play-sound')(options = {player: this.type});
 
 		this.on('input', function(msg) {
 			player.play(msg.payload|| this.file);
 			node.send(msg);
 		});
 	}
-	RED.nodes.registerType("play",PlayNode);
+	RED.nodes.registerType('play', PlayNode);
 }
